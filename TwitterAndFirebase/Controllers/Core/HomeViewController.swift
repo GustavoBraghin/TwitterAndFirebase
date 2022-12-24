b0400900c0fee54d6fc55b0b03688d785b41b266
GustavoBraghin <gugabraghin@gmail.com>
2022-12-24 11:29:56 -0300

    created custom TableViewCell with image

--- TwitterAndFirebase/Controllers/Core/HomeViewController.swift
+++ TwitterAndFirebase/Controllers/Core/HomeViewController.swift
@@ -11,7 +11,7 @@ class HomeViewController: UIViewController {
 
     private let timelineTableView: UITableView = {
         let tableView = UITableView()
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.register(TweetTableViewCell.self, forCellReuseIdentifier: TweetTableViewCell.identifier)
         return tableView
     }()
     
@@ -35,8 +35,7 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "Tweet"
+        let cell = tableView.dequeueReusableCell(withIdentifier: TweetTableViewCell.identifier, for: indexPath)
         return cell
     }
     
---
+++ TwitterAndFirebase/Views/TweetTableViewCell.swift
@@ -0,0 +1,48 @@
+//
+//  tweetTableViewCell.swift
+//  TwitterAndFirebase
+//
+//  Created by Gustavo da Silva Braghin on 24/12/22.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+    static let identifier = "tweetTableViewCell"
+    
+    private let avatarImageView: UIImageView = {
+        
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 25
+        imageView.layer.masksToBounds = true
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(systemName: "person")
+        imageView.backgroundColor = .blue
+        return imageView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(avatarImageView)
+        
+        configureConstraints()
+    }
+
+    private func configureConstraints() {
+        let avatarImageViewConstraints = [
+            avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            avatarImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 14),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 50),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 50)
+        ]
+        
+        NSLayoutConstraint.activate(avatarImageViewConstraints)
+        
+    }
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
