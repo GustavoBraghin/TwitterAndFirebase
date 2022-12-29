c7be77e5169310f37346ab128e4341857ca34ca0
GustavoBraghin <gugabraghin@gmail.com>
2022-12-29 12:27:41 -0300

    added profileTableView and created profileTableViewHeader

---
+++ TwitterAndFirebase/Views/ProfileTableViewHeaderView.swift
@@ -0,0 +1,145 @@
+//
+//  ProfileHeaderView.swift
+//  TwitterAndFirebase
+//
+//  Created by Gustavo da Silva Braghin on 29/12/22.
+//
+
+import UIKit
+
+class ProfileTableViewHeaderView: UIView {
+    
+    private let verticalSpacing: CGFloat = 5
+    
+    private let profileHeaderImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = UIImage(named: "header")
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let profileAvatarImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.clipsToBounds = true
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = 40
+        imageView.image = UIImage(systemName: "person")
+        imageView.backgroundColor = .yellow
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let displayNameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Gustavo"
+        label.textColor = .label
+        label.font = .systemFont(ofSize: 22, weight: .bold)
+        return label
+    }()
+    
+    private let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "@gbraghin"
+        label.textColor = .secondaryLabel
+        label.font = .systemFont(ofSize: 18, weight: .regular)
+        return label
+    }()
+    
+    private let userBioLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "iOS Developer from Apple and Tesla"
+        label.numberOfLines = 3
+        label.textColor = .label
+        return label
+    }()
+    
+    private let joinDateImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(systemName: "calendar", withConfiguration: UIImage.SymbolConfiguration(pointSize: 14))
+        imageView.tintColor = .secondaryLabel
+        return imageView
+    }()
+    
+    private let joinDateLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Joined December 2022"
+        label.textColor = .secondaryLabel
+        label.font = .systemFont(ofSize: 14, weight: .regular)
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(profileHeaderImageView)
+        addSubview(profileAvatarImageView)
+        addSubview(displayNameLabel)
+        addSubview(usernameLabel)
+        addSubview(userBioLabel)
+        addSubview(joinDateImageView)
+        addSubview(joinDateLabel)
+        configureConstraints()
+    }
+    
+    private func configureConstraints() {
+    
+        let profileHeaderImageViewConstraints = [
+            profileHeaderImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: -3),
+            profileHeaderImageView.topAnchor.constraint(equalTo: topAnchor),
+            profileHeaderImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 3),
+            profileHeaderImageView.heightAnchor.constraint(greaterThanOrEqualToConstant: 180)
+        ]
+        
+        let profileAvatarImageViewConstraints = [
+            profileAvatarImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            profileAvatarImageView.centerYAnchor.constraint(equalTo: profileHeaderImageView.bottomAnchor, constant: 10),
+            profileAvatarImageView.widthAnchor.constraint(equalToConstant: 80),
+            profileAvatarImageView.heightAnchor.constraint(equalToConstant: 80)
+        ]
+        
+        let displayNameLabelConstraints = [
+            displayNameLabel.leadingAnchor.constraint(equalTo: profileAvatarImageView.leadingAnchor),
+            displayNameLabel.topAnchor.constraint(equalTo: profileAvatarImageView.bottomAnchor, constant: 20)
+        ]
+        
+        let userNameLabelConstraints = [
+            usernameLabel.leadingAnchor.constraint(equalTo: displayNameLabel.leadingAnchor),
+            usernameLabel.topAnchor.constraint(equalTo: displayNameLabel.bottomAnchor, constant: verticalSpacing)
+        ]
+        
+        let userBioLabelConstraints = [
+            userBioLabel.leadingAnchor.constraint(equalTo: displayNameLabel.leadingAnchor),
+            userBioLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -5),
+            userBioLabel.topAnchor.constraint(equalTo: usernameLabel.bottomAnchor, constant: verticalSpacing)
+        ]
+        
+        let joinDateImageViewConstraints = [
+            joinDateImageView.leadingAnchor.constraint(equalTo: displayNameLabel.leadingAnchor),
+            joinDateImageView.topAnchor.constraint(equalTo: userBioLabel.bottomAnchor, constant: verticalSpacing)
+        ]
+        
+        let joinDateLabelConstraints = [
+            joinDateLabel.leadingAnchor.constraint(equalTo: joinDateImageView.trailingAnchor, constant: 2),
+            joinDateLabel.bottomAnchor.constraint(equalTo: joinDateImageView.bottomAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(profileHeaderImageViewConstraints)
+        NSLayoutConstraint.activate(profileAvatarImageViewConstraints)
+        NSLayoutConstraint.activate(displayNameLabelConstraints)
+        NSLayoutConstraint.activate(userNameLabelConstraints)
+        NSLayoutConstraint.activate(userBioLabelConstraints)
+        NSLayoutConstraint.activate(joinDateImageViewConstraints)
+        NSLayoutConstraint.activate(joinDateLabelConstraints)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
