4f77b1f5e2305f320572f860588be990050fc6a6
Gustavo Braghin <gugabraghin@gmail.com>
2023-02-14 11:49:27 -0300

    feat: dismissing onboardingVC and created loginVC file

---
+++ TwitterAndFirebase/Controllers/Onboarding/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  TwitterAndFirebase
+//
+//  Created by Gustavo da Silva Braghin on 14/02/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
