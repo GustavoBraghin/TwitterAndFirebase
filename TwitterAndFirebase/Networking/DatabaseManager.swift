4de834cd79d0deae498e6ad3bdc6b8cefa5a7d8c
Gustavo Braghin <gugabraghin@gmail.com>
2023-02-15 14:21:37 -0300

    feat: created twitterUser model and databaseManager

---
+++ TwitterAndFirebase/Networking/DatabaseManager.swift
@@ -0,0 +1,29 @@
+//
+//  DatabaseManager.swift
+//  TwitterAndFirebase
+//
+//  Created by Gustavo da Silva Braghin on 15/02/23.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestoreSwift
+import FirebaseFirestoreCombineSwift
+import Combine
+
+class DatabaseManager {
+    
+    static let shared = DatabaseManager()
+    
+    let db = Firestore.firestore()
+    let usersPath: String = "users"
+    
+    func collectionUsers(add user: User) -> AnyPublisher<Bool, Error> {
+        let twitterUser = TwitterUser(from: user)
+        return db.collection(usersPath).document(twitterUser.id).setData(from: twitterUser)
+            .map { _ in
+                return true
+            }
+            .eraseToAnyPublisher()
+    }
+}
